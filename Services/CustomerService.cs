be512ac80caf68dc68d3faa146a430d7c8a17874
Paul Shpilsher <paul.shpilsher@gmail.com>
2021-02-23 22:47:03 +0300

    CustomerService implementation

--- Services/CustomerService.cs
+++ Services/CustomerService.cs
@@ -1,8 +1,35 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LiquidApi.Factories;
+using LiquidApi.Repositories;
+
 namespace LiquidApi.Services
 {
     public class CustomerService
     {
-        
-    }
+        private readonly CustomerFactory _customerFactory;
+        private readonly CustomerRepository _customerRepository;
+        private readonly AddressRepository _addressRepository;
 
+        public CustomerService(CustomerRepository customerRepository, AddressRepository addressRepository, CustomerFactory customerFactory)
+        {
+            _customerRepository = customerRepository;
+            _addressRepository = addressRepository;
+            _customerFactory = customerFactory;
+        }
+
+        public async Task<List<Controllers.Responses.Address>> GetAddressesByCountry(string country)
+        {
+            var addresses =  await _addressRepository.GetAddressByCountry(country);
+            return addresses.Select(x => _customerFactory.CreateAddressResponse(x)).ToList();
+        }
+
+
+        public async Task<Controllers.Responses.Customer> GetCustomerByFirstAndLastName(string firstName, string lastName)
+        {
+            var customer =  await _customerRepository.GetCustomerByFirstAndLastName(firstName, lastName);
+            return customer == null ? null : _customerFactory.CreateCustomerResponse(customer);
+        }
+    }
 }
