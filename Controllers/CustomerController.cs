b089a6e63dbf8d2c1a29e70234a8b5d0cc88b917
Paul Shpilsher <paul.shpilsher@gmail.com>
2021-02-23 23:13:48 +0300

    CustomerController.GetCustomerByFirstAndLastName

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LiquidApi.Services;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -12,11 +13,33 @@ namespace LiquidApi.Controllers
     public class CustomerController : ControllerBase
     {
         private readonly ILogger<CustomerController> _logger;
+        private readonly CustomerService _service;
 
-        public CustomerController(ILogger<CustomerController> logger)
+        public CustomerController(
+            ILogger<CustomerController> logger,
+            CustomerService service)
         {
             _logger = logger;
+            _service = service;
         }
+
+        [HttpPost("searches")]
+        public async Task<ActionResult<Responses.Customer>> GetCustomerByFirstAndLastName(Requests.CustomerByFirstAndLastName req)
+        {
+            if(req == null)
+            {
+                return BadRequest();
+            }
+
+            var customer = await _service.GetCustomerByFirstAndLastName(req.FirstName, req.LastName);
+            if(customer == null)
+            {
+                return NoContent();
+            }
+
+            return customer;
+        }
+
 
     }
 }
