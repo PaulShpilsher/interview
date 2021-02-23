aea25664142b2da2985a4ac8c445194bee28e1ec
Paul Shpilsher <paul.shpilsher@gmail.com>
2021-02-23 20:43:42 +0300

    completed during an interview

---
+++ Repositories/7AddressRepositorry.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LiquidApi.Context;
+
+namespace LiquidApi.Repositories
+{
+    public class AddressRepository
+    {
+        private readonly LiquidApiContext _context;
+        public AddressRepository(LiquidApiContext context) 
+        {
+            _context = context;
+        }
+
+        public List<Models.Address> GetAddressByCountry(string country)
+        {
+            var addresses = _context.Addresses.Where(x => x.Country == country);
+            if(addresses.Any())
+            {
+                return addresses.ToList();
+            }
+            else
+            {
+                return new List<Models.Address>(0);
+            }
+            
+        }
+    }
+}
--- Repositories/CustomerRepository.cs
+++ Repositories/CustomerRepository.cs
@@ -2,10 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LiquidApi.Context;
 
 namespace LiquidApi.Repositories
 {
     public class CustomerRepository
     {
+        private readonly LiquidApiContext _context;
+        public CustomerRepository(LiquidApiContext context) 
+        {
+            _context = context;
+        }
+    
+
+        public Models.Customer GetCustomerByFirstAndLastName(string fistName, string LastName)
+        {
+            return _context.Customers.FirstOrDefault( x=> (x.FirstName == fistName && x.LastName == LastName));
+        }
+
+
+        public Models.Customer GetCustomerById(Guid id)
+        {
+            return _context.Customers.FirstOrDefault( x=> x.Id == id);
+        }
     }
 }
